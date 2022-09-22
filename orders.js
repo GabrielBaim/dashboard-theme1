335df628209c0ab736e06e525b6a1941e86d7260
GabrielBaim <gabriel.baima16@gmail.com>
2022-09-22 15:49:36 -0300

    first commit

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85931",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "LAVENDER KF102 Drone",
+    productNumber: "7541",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "1052",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "9521",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "2502",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "DJI Air 25",
+    productNumber: "5124",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lorense Drone",
+    productNumber: "8547",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
